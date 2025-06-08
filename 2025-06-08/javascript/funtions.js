9f5c25f0ac20869f3a3ec2f2e6b397127e2db1a9
Manul Perera <perera102at@u.agri.ruh.ac.lk>
2025-06-08 16:32:25 +0530

    Add files via upload

---
+++ 2025-06-08/javascript/funtions.js
@@ -0,0 +1,6 @@
+function myfuntion(a,b) {
+    return a*b;
+}
+
+let x = myfuntion(4,6);
+document.getElementById("demo5").innerHTML = "the value of x is: " + x;
---
+++ 2025-06-08/javascript/variables.js
@@ -0,0 +1,20 @@
+// JavaScript Variables can be declared in 4 ways:
+
+// Automatically
+// Using var
+// Using let
+// Using const
+
+// Automatically
+
+x = 2;
+y = 3;
+z = x + y;
+document.getElementById("demo3").innerHTML = "the value of z is:" + z;
+
+var a = 5;
+var b = 6;
+var c = a + b;
+
+document.getElementById("demo4").innerHTML = "the value of c is:" + c;
+
